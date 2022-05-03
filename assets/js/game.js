9973a7699f2572189928ec2c0c6246141a81a027
Glitchii <hello@tempfile.site>
2022-05-03 23:16:55 +0100

    Make aliens move, centre initial alient ship, text, and player

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,17 +25,9 @@ const keyup = event => {
 }
 
 
-const move = (player, skyHeight) => {
+const move = (player, skyHeight, screenWidth) => {
 	const positionLeft = player.offsetLeft;
 	const positionTop = player.offsetTop;
-
-	if (leftPressed)
-		player.style.left = positionLeft - 1 + 'px',
-			player.className = 'character walk left';
-
-	if (rightPressed)
-		player.style.left = positionLeft + 1 + 'px',
-			player.className = 'character walk right';
 
 	if (upPressed) {
 		// Don't go above the sky. 'document.elementFromPoint' is a bit buggy.
@@ -54,6 +46,21 @@ const move = (player, skyHeight) => {
 			player.className = 'character walk down';
 	}
 
+
+
+	if (leftPressed) {
+		player.style.left = (positionLeft <= 0 ? 0 : positionLeft - 1) + 'px';
+
+		if (!upPressed && !downPressed)
+			player.className = 'character walk left';
+	}
+
+	if (rightPressed) {
+		player.style.left = (positionLeft >= screenWidth - player.offsetWidth ? screenWidth - player.offsetWidth : positionLeft + 1) + 'px';
+
+		if (!upPressed && !downPressed)
+			player.className = 'character walk right';
+	}
 }
 
 
@@ -67,15 +74,20 @@ const keydown = event => {
 	else if (downKeys.includes(event.which))
 		downPressed = downKeys.includes(event.which);
 }
+const blink = (subject, duration = 1000, times = 3) => {
+	return subject.animate(Array(times).fill([{ opacity: '.5' }, { opacity: '1' }]).flat(), { duration: duration, })
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-	var playing,
+	let spawnInterval = 1000, playing,
 		player = document.getElementById('player'),
 		startBtn = document.querySelector('.start'),
 		sky = document.querySelector('.sky'),
 		skyHeight = sky.offsetHeight,
-		timeout = setInterval(move, 10, player, skyHeight),
-		mainAlien = document.querySelector('#alien.main');
+		skyWidth = sky.offsetWidth,
+		timeout = setInterval(move, 10, player, skyHeight, skyWidth),
+		health = document.querySelector('.health'),
+		mainAlien = document.querySelector('.alien.main');
 
 	document.addEventListener('keydown', keydown);
 	document.addEventListener('keyup', keyup);
@@ -83,7 +95,80 @@ document.addEventListener('DOMContentLoaded', () => {
 	startBtn.addEventListener('click', e => {
 		playing = !document.body.classList.add('playing');
 		// Animate aliet above sky and hide it
-		mainAlien.animate([{ transform: 'translateY(-250%)' }], { duration: 500, })
+		mainAlien.animate([{ transform: 'translate(-50%, -250%)' }], { duration: 500, })
 			.onfinish = () => mainAlien.classList.add('hidden');
+
+		// Blink animate the player
+		health.classList.remove('hidden');
+		blink(health);
+		// blink(player, 600);
+
+		player.classList.add('blink');
+
+		const newAlien = duration => {
+			setTimeout(newAlien, spawnInterval);
+			const alien = document.createElement('div');
+			sky.appendChild(alien);
+			console.log(alien);
+
+			alien.className = 'alien';
+			// Spawn alien inside the sky
+			// const randomY = Math.random() * (skyHeight - alien.offsetHeight) + 'px';
+			const randomY = Math.random() * (skyHeight - skyHeight / 2 - alien.offsetHeight);
+			const randomX = Math.random() * (skyWidth - alien.offsetWidth);
+			alien.style.left = randomX + 'px';
+			// alien.style.top = randomY;
+			// Animate top down
+			alien.animate([{ opacity: 0, top: '-100%' }, { top: randomY + 'px' }], { duration: duration || 5000, easing: 'ease-in-out' })
+				.onfinish = () => {
+					alien.style.top = randomY + 'px';
+					// Turn around the ship and go back up
+					// setTimeout(() => {
+					const stay = Math.random() > .5;
+					// const transform = Math.random() > .5  ? 'rotate(180deg)' : 'rotate(0deg)';
+					const transform = 'rotate(180deg)';
+					alien.animate([
+						{ opacity: 1, top: randomY + 'px' },
+						{ transform },
+						{ top: '-100%' }], { duration: 2000, easing: 'ease-in-out', fill: 'forwards' })
+							.onfinish = () => alien.remove();
+							
+					// alien.animate([
+					// 	{ opacity: 1, top: randomY + 'px' },
+					// 	{ transform },
+					// ], { duration: 1000, easing: 'ease-in-out' }).onfinish = () => {
+					// 	alien.style.transform = transform;
+					// 	alien.animate([{ top: '-100%' },], { duration: 2000, easing: 'ease-in-out', fill: 'forwards' })
+					// 		.onfinish = () => alien.remove();
+					// }
+					// }, Math.random() * 5000);
+				}
+
+			// alien.style.top = skyHeight + 'px';
+			// alien.style.left = Math.random() * skyWidth + 'px';
+
+
+			// alien.style.top = Math.random() * skyWidth + 'px';
+			// document.body.appendChild(alien);
+			// Animate alien down and remove it
+			// alien.animate([{ transform: 'translate(-50%, -250%)' }], { duration: Math.random() * 5000 + 5000, })
+			// 	.onfinish = () => {
+			// 		// alien.remove();
+			// 		// newAlien();
+			// 	}
+			// Finish later
+		}
+
+		// Spawn alien ships
+		newAlien(0);
+		// setInterval(() => {
+		// 	spawnInterval = spawnInterval - 100;
+		// 	console.log(spawnInterval);
+		// 	window.spawnInterval = spawnInterval;
+		// }, 1000);
+		// setTimeout(newAlien(), spawnInterval);
+		spawnInterval;
 	});
+
+	// startBtn.click();
 });
